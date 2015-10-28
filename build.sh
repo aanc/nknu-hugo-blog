3e902b60b615231e39f5d22b4a5030dc39084e59
Adrien Anceau <adrien.anceau@gmail.com>
2015-10-28 16:03:48 +0100

    Added deployment automation logic

--- build.sh
+++ build.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+staticSiteRepo="http://github.com/aanc/aanc.github.io.git"
+
 deploy() {
 	echo
-	echo "--> Deploying ..."
-	echo "Todo: deploy to github pages"
+	echo "--> Deploying to $staticSiteRepo ..."
+
+	local commitMessage="Manual use of build.sh"
+
+	cd public/
+	if [[ -n $TRAVIS ]]; then
+		git config user.name "Travis" && git config user.email travis@nknu.net
+		commitMessage="Travis build #${TRAVIS_BUILD_NUMBER}"
+	fi
+
+	git status
+	git add .
+	git commit -m "${commitMessage}"
+	git push origin master
 }
 
 build() {
+	echo
+	echo "--> Cloning existing site from $staticSiteRepo ..."
+	git clone $staticSiteRepo public/
+
 	echo
 	echo "--> Building ..."
 	$GOPATH/bin/hugo
@@ -15,12 +33,16 @@ build() {
 clean() {
 	echo
 	echo "--> Cleaning ..."
-	[[ -d public ]] && rm -fr public/*
+	[[ -d public ]] && rm -fr public
 }
+
+[[ $1 == deploy ]] && DO_DEPLOY=yup
 
 clean
 build
-[[ $TRAVIS_PULL_REQUEST == false ]] && [[ $TRAVIS_BRANCH == master ]] && deploy
+if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == master ]] || [[ -n $DO_DEPLOY ]]; then
+	deploy
+fi
 
 echo
 echo "The end."
